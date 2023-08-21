e5161fb885a955cd48e29ca5a493ce130e38e5ec
Vinicius Gouvea <vrgouvea@outlook.com>
2023-08-21 19:00:04 -0300

    routes fixed

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -46,8 +46,9 @@ class NotesController{
   }
 
   async index(request, response){
-    const { title, user_id, tags} = request.query;
-
+    const { title, tags} = request.query;
+    const user_id = request.user.id;
+    
     let notes;
 
     if(tags){
